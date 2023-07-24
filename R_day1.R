afb39ad7ea536a56512714ebbf532abebb87960e
vishalbhil1 <140387109+vishalbhil1@users.noreply.github.com>
2023-07-24 14:29:35 +0530

    Add files via upload

---
+++ R_day1.R
@@ -0,0 +1,25 @@
+a <- 10+18
+a
+b <- 'vishal'
+class(b)
+A <- 100L
+class(A)
+B <- 5
+class(B)
+x <- 10
+y <- 20
+z=x+y
+z
+D <- T
+class(D)
+S <- 17.8
+class(S)
+#accessing elements from vector
+
+dates <- c(21,1,17,15,3)
+month <- c('april','march','august','june')
+info <- c(21,'april',21.4,T)
+info[3]
+month[1] 
+month[c(1,4)]
+info[2:4]
